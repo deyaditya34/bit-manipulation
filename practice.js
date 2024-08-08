bb8b5d4e9d093bd33c82d942410f79b14d2cbc70
Aditya Dey <deyaditya34@gmail.com>
2024-08-08 13:48:28 +0530

    finished some bit calculation problems

--- practice.js
+++ practice.js
@@ -1,28 +1,25 @@
-const { decimalToBinary } = require("./decimalToBinary");
-const { binaryToDecimal } = require("./binaryToDecimal");
+const obj = {
+  name: "name",
+  color: "color",
+  city: "city",
+};
 
-const a = 0b00;
-let b = 0b10;
-const d = 0b0110;
- c = a | b;
-console.log(decimalToBinary(a));
-console.log(decimalToBinary(b));
-console.log("c -", decimalToBinary(c));
+function stringifyObject(obj, index = 0, result = {}) {
+  const objKeys = Object.keys(obj);
 
-// [3, 2, 1, 13, 4, 5, 6, 13, 4, 3,5, 2, 1]
+  const resultKeys = objKeys.slice(index, objKeys.length);
 
-/**
-11 ^ 0 = 11
-11 ^ 10 = 01
-01 ^ 01 = 0
-1101 ^ 0000 = 1101
-1101 ^ 100 = 1001
-1001 ^ 101 = 1100
-1100 ^ 110 = 1010
-1010 ^ 1101 = 0111
-0111 ^ 100 = 0011
-0011 ^ 0011 = 0
-0 ^ 101 = 101
-101 ^ 010 = 111
-111 ^ 001 = 110
-*/
+  if (resultKeys.length <= 0) {
+    return result;
+  }
+  
+  const resultKey = resultKeys[0];
+  const resultKeyValue = Reflect.get(obj, resultKey);
+
+  Reflect.set(result, `${ resultKey.toString() }`, resultKeyValue.toString());
+  console.log(result)
+
+  return stringifyObject(obj, ++index, result);
+}
+
+console.log(stringifyObject(obj));
